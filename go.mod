efa79ac9fcce225cfcf0ea86c46d0006d406b748
M1000 <diabatluffy@gmail.com>
2025-10-09 00:50:33 +0800

    It kinda works now

--- go.mod
+++ go.mod
@@ -17,18 +17,21 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/lengzuo/supa v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
 	github.com/spf13/afero v1.14.0 // indirect
 	github.com/spf13/cast v1.9.2 // indirect
 	github.com/tdewolff/parse/v2 v2.8.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20250819193227-8b4c13bb791b // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,229 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	supabase "github.com/lengzuo/supa"
 )
+
+type Movie struct {
+	ID          int      `json:"id"`
+	MovieID     int      `json:"movieID"`
+	Title       string   `json:"title"`
+	Poster      string   `json:"poster"`
+	Director    string   `json:"director"`
+	Cast        []string `json:"cast"`
+	Review      string   `json:"review"`
+	Genres      []string `json:"genres"`
+	Watched     bool     `json:"watched"`
+	PlanToWatch bool     `json:"planToWatch"`
+	// Released    string    `json: "released"`
+	CreatedAt time.Time `json: "createdAt"`
+}
+
+var client *supabase.Client
 
 func main() {
 	app := fiber.New()
 	fmt.Println("Hello World!")
-	app.Listen(":8080")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	conf := supabase.Config{
+		// Your project api key, you can use either `anon` or `service_role`.
+		// but i will suggest you to use `service_role` as your api key and keep it secret.
+		ApiKey: os.Getenv("SUPABASE_API_KEY"),
+		// Retrieve your project ref from project url
+		// eg: https://this-your-project-ref.supabase.co
+		ProjectRef: os.Getenv("SUPABASE_PROJECT_REF"),
+		// Set it `false` in production to avoid extra log print.
+		Debug: true,
+	}
+
+	client, err := supabase.New(conf)
+
+	if err != nil {
+		fmt.Println("failed in init supa client: ", err)
+		return
+	}
+
+	PORT := os.Getenv("PORT")
+
+	//Movies
+	app.Get("/api/movies", func(c *fiber.Ctx) error { return getMovies(c, client) })
+	app.Get("/api/movies/:id", getMovieByID)
+	app.Post("/api/movies/:id", createMovie)
+	app.Patch("/api/movies/:id", updateMovie)
+	app.Delete("/api/movies/:id", deleteMovie)
+
+	//Books
+	// app.Get("/api/books",getBooks)
+	// app.Get("/api/books/:id",getBookByID)
+	// app.Post("/api/books/:id",createBook)
+	// app.Patch("/api/books/:id",updateBook)
+	// app.Delete("/api/books/:id",deleteBook)
+
+	// //TV Shows
+	// app.Get("/api/shows",getShows)
+	// app.Get("/api/shows/:id",getShowByID)
+	// app.Post("/api/shows/:id",createShow)
+	// app.Patch("/api/shows/:id",updateShow)
+	// app.Delete("/api/shows/:id",deleteShow)
+
+	// //Game
+	// app.Get("/api/games",getGame)
+	// app.Get("/api/games/:id",getGameByID)
+	// app.Post("/api/games/:id",createGame)
+	// app.Patch("/api/games/:id",updateGame)
+	// app.Delete("/api/games/:id",deleteGame)
+
+	// if PORT == "" {
+	// 	PORT = "5000"
+	// }
+	log.Fatal(app.Listen(":" + PORT))
 }
+
+func getMovies(c *fiber.Ctx, client *supabase.Client) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var movies []Movie
+	if err := client.DB.From("movies").Select("*").Execute(ctx, &movies); err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(movies)
+}
+
+func getMovieByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var movies []Movie
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := client.DB.From("movies").Select("*").Eq("id", id).Execute(ctx, &movies)
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	if len(movies) == 0 {
+		return c.Status(404).JSON(fiber.Map{"error": "Movie not found"})
+	}
+
+	return c.JSON(movies[0])
+}
+
+func createMovie(c *fiber.Ctx) error {
+	var movie Movie
+
+	if err := c.BodyParser(movie); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var inserted []Movie
+
+	if err := client.DB.From("movies").Insert(movie).Execute(ctx, &inserted); err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(201).JSON(inserted)
+}
+func updateMovie(c *fiber.Ctx) error {
+	id := c.Params("id")
+	movie := new(Movie)
+
+	if err := c.BodyParser(movie); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var updatedMovie []Movie
+
+	err := client.DB.From("movies").Update(movie).Eq("id", id).Execute(ctx, &updatedMovie)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(updatedMovie)
+}
+func deleteMovie(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.DB.From("movies").Delete().Eq("id", id).Execute(ctx, nil); err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{"message": "Movie Deleted"})
+}
+
+//Books
+
+// func createBook(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func getBooks(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func getBookByID(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func updateBook(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func deleteBook(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+
+// //TV shows
+
+// func createShow(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func getShows(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func getShowByID(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func updateShow(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func deleteShow(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// //Games
+
+// func createGame(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func getGame(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func getGameByID(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func updateGame(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
+// func deleteGame(c *fiber.Ctx) error {
+// 	id := c.Params("id")
+// }
